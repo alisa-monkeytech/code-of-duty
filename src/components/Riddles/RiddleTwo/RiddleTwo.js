551352cc39f8b206e614e9b52e208b03ece4256d
Alisa Makarova <alisa@monkeytech.co.il>
2022-07-03 20:41:41 +0300

    added logic to step 2

--- src/components/Riddles/RiddleTwo/RiddleTwo.js
+++ src/components/Riddles/RiddleTwo/RiddleTwo.js
@@ -1,20 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Riddles from '../Riddles';
 import RiddleTwo_icon from '../../../assets/images/RiddleTwo_icon.svg';
 import Simon from './Simon/Simon';
 import { Label } from './RiddleTwo.style';
 import GoodJob from '../GoodJob/GoodJob';
 
+const hiddenSolution = 'URUDLLUDRL';
+
 function RiddleTwo() {
+    const [showInput, setShowInput] = useState(true);
+    const [currentSolution, setCurrentSolution] = useState('');
+    const [buttonsCombination, setButtonsCombination] = useState('');
+
+
+    useEffect(() => {
+        if(!showInput) return
+        console.log(buttonsCombination)
+        if(buttonsCombination === hiddenSolution){
+            setShowInput(false)
+            return
+        }
+        if(!hiddenSolution.startsWith(buttonsCombination)){
+            setButtonsCombination("")
+            alert("wrong!")
+
+        }
+
+    }, [buttonsCombination]);
+
+    const onButtonClick = (p) => {
+        const map = {
+            top: "U",
+            left: "D",
+            right: "R",
+            bottom: "L"
+        }
+        setButtonsCombination(buttonsCombination + map[p])
+    }
+
     return ( 
         <Riddles.Container background='#011F20'>
             <Riddles.LeftSection>
                 <Riddles.Title color='#FEA614'>Ek Chuaj</Riddles.Title>
 
-                <Simon />
+                <Simon  onButtonClick={onButtonClick}/>
 
-                {false && <Label>Simon says... find out!</Label>}
-                {true && <GoodJob color='#32CC9A'/>}
+                {showInput && <Label>Simon says... find out!</Label>}
+                {!showInput && <GoodJob color='#32CC9A'  code='54875'/>}
             </Riddles.LeftSection>
 
             <Riddles.StyledImage src={RiddleTwo_icon} alt='Decorative icon'/>
